42c51287d5047e1c1b9d3025a2d41e38fb7df692
Jasdeep Singh <j.singhhh917@gmail.com>
2022-11-22 11:48:13 +1100

    listings controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/listings", to: "listing#index"
+  # get "/listings", to: "listings#index"
+  resources :listings, only: %i[index create new show]
 end
