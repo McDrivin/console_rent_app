748dae6892002edacb4563d99070d79a99ab4279
Stephen Taylor <steponitph@gmail.com>
2022-11-22 15:24:12 +1100

    just to update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,37 @@
 class BookingsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: :home
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def create
+    @booking = Booking.new[booking_params]
+    @booking.save
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+
+  def show
+    @booking = Booking.new(booking_params)
+  end
+
+  def destroy
+    if @booking.destroy
+      redirect_to bookings_path(@booking)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def listing_params
+    params.require(:booking).permit(:title, :description, :rental_price)
+  # def booking_params
+  # params[:booking] # index create new show destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # get "/listings", to: "listings#index"
-  resources :listings, only: %i[index create new show]
+  resources :listings do
+    resources :bookings, only: [:create, :new]
+  end
+  resources :bookings, only: [:show, :destroy, :edit, :update]
 end
+  # resources :listings, only: %i[index create new show destroy]
+  #   resources :bookings, only: [ :index, :create, :new, :show, :destroy ]
