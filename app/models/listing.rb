22fbce547dfef6c4c2f9c8082a367fc15b0a1556
Janaka Vithanage <jkvithana@gmail.com>
2022-11-22 17:02:23 +1100

    Added validations to models.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  validates :title, presence: true, length: { minimum: 6 }
+  validates :description, presence: true
+  validates :rental_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
